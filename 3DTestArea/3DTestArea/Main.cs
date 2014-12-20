6009472cbbc295f92ae676ee7b25781804f94276
Josh <Josh.Hanke@gmail.com>
2014-12-19 21:17:00 -0600

    Terrain Commit without shadows

--- 3DTestArea/3DTestArea/Main.cs
+++ 3DTestArea/3DTestArea/Main.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace _3DTestArea
 {
@@ -10,8 +11,22 @@ namespace _3DTestArea
     public class Main : Game
     {
         GraphicsDeviceManager graphics;
+        GraphicsDevice device;
         SpriteBatch spriteBatch;
 
+        VertexPositionColor[] vertices;
+        int[] indices;
+
+        Effect effects;
+        Matrix viewMatrix;
+        Matrix projectionMatrix;
+
+        private float angle = 0f;
+
+        private int terrainWidth = 4;
+        private int terrainHeight = 3;
+
+        private float[,] heightData;
         public Main()
             : base()
         {
@@ -28,7 +43,11 @@ namespace _3DTestArea
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            Window.Title = "Riemer's XNA Tutorials -- 3D Series 1";
             base.Initialize();
         }
 
@@ -40,7 +59,16 @@ namespace _3DTestArea
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            device = graphics.GraphicsDevice;
+            //code here is to make xna custom fx work with monogame fx
+            byte[] bytecode = File.ReadAllBytes("Content/Effects/effects.mgfx");
+            effects = new Effect(device, bytecode);
 
+            SetUpCamera();
+            Texture2D heightMap = Content.Load<Texture2D>("Terrain/heightmap");
+            LoadHeightData(heightMap);
+            SetUpVertices();
+            SetUpIndices();
             // TODO: use this.Content to load your game content here
         }
 
@@ -50,7 +78,6 @@ namespace _3DTestArea
         /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
@@ -62,7 +89,11 @@ namespace _3DTestArea
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.E))
+                angle += 0.05f;
+            if (keyState.IsKeyDown(Keys.Q))
+                angle -= 0.05f;
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -74,11 +105,114 @@ namespace _3DTestArea
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
+
+            //These lines take off CullMode so that back triangles are rendered
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            rs.FillMode = FillMode.Solid;
+            device.RasterizerState = rs;
+            Matrix worldMatrix = Matrix.CreateTranslation(-terrainWidth / 2.0f, 0, terrainHeight / 2.0f) * Matrix.CreateRotationY(angle);
+
+            effects.CurrentTechnique = effects.Techniques["ColoredNoShading"];
+            effects.Parameters["xView"].SetValue(viewMatrix);
+            effects.Parameters["xProjection"].SetValue(projectionMatrix);
+            effects.Parameters["xWorld"].SetValue(worldMatrix);
 
             // TODO: Add your drawing code here
-
+            foreach (EffectPass pass in effects.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, vertices, 0, vertices.Length, indices, 0, indices.Length / 3, VertexPositionColor.VertexDeclaration);
+            }
             base.Draw(gameTime);
+        }
+        private void SetUpVertices()
+        {
+            float minHeight = float.MaxValue;
+            float maxHeight = float.MinValue;
+            for (int x = 0; x < terrainWidth; x++)
+            {
+                for (int y = 0; y < terrainHeight; y++)
+                {
+                    if (heightData[x, y] < minHeight)
+                        minHeight = heightData[x, y];
+                    if (heightData[x, y] > maxHeight)
+                        maxHeight = heightData[x, y];
+                }
+            }
+             vertices = new VertexPositionColor[terrainWidth * terrainHeight];
+             for (int x = 0; x < terrainWidth; x++)
+             {
+                 for (int y = 0; y < terrainHeight; y++)
+                 {
+                     vertices[x + y * terrainWidth].Position = new Vector3(x, heightData[x, y], -y);
+
+                     if (heightData[x, y] < minHeight + (maxHeight - minHeight) / 4)
+                         vertices[x + y * terrainWidth].Color = Color.Blue;
+                     else if (heightData[x, y] < minHeight + (maxHeight - minHeight) * 2 / 4)
+                         vertices[x + y * terrainWidth].Color = Color.Green;
+                     else if (heightData[x, y] < minHeight + (maxHeight - minHeight) * 3 / 4)
+                         vertices[x + y * terrainWidth].Color = Color.Brown;
+                     else
+                         vertices[x + y * terrainWidth].Color = Color.White;
+                 }
+             }
+        }
+        private void SetUpIndices()
+        {
+            indices = new int[(terrainWidth - 1) * (terrainHeight - 1) * 6];
+            int counter = 0;
+            for (int y = 0; y < terrainHeight - 1; y++)
+            {
+                for (int x = 0; x < terrainWidth - 1; x++)
+                {
+                    int lowerLeft = x + y * terrainWidth;
+                    int lowerRight = (x + 1) + y * terrainWidth;
+                    int topLeft = x + (y + 1) * terrainWidth;
+                    int topRight = (x + 1) + (y + 1) * terrainWidth;
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = lowerRight;
+                    indices[counter++] = lowerLeft;
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = topRight;
+                    indices[counter++] = lowerRight;
+                }
+            }
+        }
+        private void SetUpCamera()
+        {
+            viewMatrix = Matrix.CreateLookAt(new Vector3(60, 80, -80), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 1.0f, 300.0f);
+        }
+        private void LoadHeightData(Texture2D heightMap)
+        {
+            terrainWidth = heightMap.Width;
+            terrainHeight = heightMap.Height;
+
+            Color[] heightMapColors = new Color[terrainWidth * terrainHeight];
+            heightMap.GetData(heightMapColors);
+
+            heightData = new float[terrainWidth, terrainHeight];
+            for (int x = 0; x < terrainWidth; x++)
+                for (int y = 0; y < terrainHeight; y++)
+                    heightData[x, y] = heightMapColors[x + y * terrainWidth].R / 5.0f;
+        }
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
         }
     }
 }
